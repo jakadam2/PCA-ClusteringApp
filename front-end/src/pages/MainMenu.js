3e4532d200ccabed6159253ac06bee094eec3c84
Aleksandra Smela <aleksandra.smela@gmail.com>
2024-03-24 13:47:09 +0100

    Init the rest of GUI

---
+++ front-end/src/pages/MainMenu.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const MainMenu = () => {
+
+  return (
+    <div className="relative h-[450px]">
+      <Fragment>abc</Fragment>
+    </div>
+  );
+};
+
+export default MainMenu;
---
+++ front-end/src/pages/MainPCAComponentsVisualization.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const MainPCAComponentsVisualization = () => {
+
+  return (
+    <div className="relative h-[450px]">
+      <Fragment>def</Fragment>
+    </div>
+  );
+};
+
+export default MainPCAComponentsVisualization;
