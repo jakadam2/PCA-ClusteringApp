5f2b8ea84cb7655b251fc47d58d747ecfbbf88fb
MasterHamster42 <115225781MasterHamster42@users.noreply.github.com>
2024-03-25 22:51:29 +0100

    Add api swagger documentation

--- backend/api/api.py
+++ backend/api/api.py
@@ -14,77 +14,138 @@ from backend.pca import PCA
 router = APIRouter(prefix="/api")
 
 
-@router.post("/file")
+@router.post("/file", summary="Upload a dataset")
 async def post_dataset(file: UploadFile = File(...)):
-    df = pd.read_csv(file.file, sep=';')
+    """
+    ## Upload a dataset file in CSV format.
+
+    Uploaded dataset becomes currently active dataset.
+    The CSV file should be delimited by semicolons (;).
+    """
+    df = pd.read_csv(file.file, sep=';', decimal=",")
     DataSet()._data_set = df
-    print(df.head())
 
 
-@router.get("/file", response_model=DatasetSchema)
+@router.get("/file", summary="Retrieve the dataset", response_model=DatasetSchema)
 async def get_dataset():
+    """
+    ## Retrieve the entire dataset schema.
+
+    This endpoint returns the schema of the currently active dataset.
+    """
     return DatasetSchema.from_data_frame(DataSet().data)
 
 
-@router.get("/dataset", response_model=DatasetSchema)
+@router.get("/dataset", summary="Get dataset head", response_model=DatasetSchema)
 async def get_head(rows: Annotated[int, Query(gt=1)]):
+    """
+    ## Get the first 'n' rows of the dataset.
+    """
     head = DataSet().get_head(rows).fillna('')
     return DatasetSchema.from_data_frame(head)
 
 
-@router.put("/dataset/columns_names")
+@router.put("/dataset/columns_names", summary="Update column names")
 async def update_columns_names(input_schema: UpdateColumnNames):
+    """
+    ## Update the names of the dataset columns.
+
+    Renames dataset's columns according to the given mapping of old column names to new column names.
+    """
     transformed_data = DataTransformer.rename(DataSet().data, input_schema.mapping)
     DataSet().data = transformed_data
 
 
-@router.put("/dataset/columns_types")
+@router.put("/dataset/columns_types", summary="Update column types")
 async def update_columns_types(input_schema: UpdateColumnTypes):
+    """
+    ## Update the data types of the dataset columns.
+
+    Change types of columns according to the given mapping of column names to new Datatypes. This not only changes the
+    displayed type of the column but slo tries to transform the data to the desired type.
+    """
     transformed_data = DataTransformer.change_types(DataSet().data, input_schema.mapping)
     DataSet().data = transformed_data
 
 
-@router.get("/pca/graph", response_model=Graph)
+@router.get("/pca/graph", summary="PCA components graph", response_model=Graph)
 async def get_components_graph():
+    """
+    ## Retrieve a graph of PCA components.
+
+    This endpoint returns a graph image that visualizes the Principal Component Analysis (PCA) components
+    of the currently active dataset.
+    """
     graph = PCA.components_graph(DataSet().data, DataSet().age)
     return Response(graph.getvalue(), media_type='image/png')
 
 
-@router.get('/pca/transform', response_model=DatasetSchema)
+@router.get('/pca/transform', summary="PCA transformation result", response_model=DatasetSchema)
 async def get_pca():
+    """
+    ## Get the result of PCA transformation.
+
+    This endpoint applies PCA transformation to the current active dataset and returns the transformed dataset schema.
+    """
     transformed_data = PCA.transform(DataSet().data, DataSet().age)
     return DatasetSchema.from_data_frame(transformed_data)
 
 
-@router.put('/pca/transform')
+@router.put('/pca/transform', summary="Perform PCA")
 async def perform_pca():
+    """
+    ## Perform PCA transformation on the dataset.
+
+    This endpoint applies PCA transformation to the current active dataset,
+    and sets the result as current active dataset.
+    """
     transformed_data = PCA.transform(DataSet().data, DataSet().age)
     DataSet().data = transformed_data
 
 
-@router.get("/normalization/methods", response_model=list[NormalizationType])
+@router.get("/normalization/methods", summary="Normalization methods", response_model=list[NormalizationType])
 async def get_normalization_methods():
+    """
+    ## List all available normalization methods.
+    """
     methods = DataTransformer.get_normalization_methods()
     return [NormalizationType.model_validate(method) for method in methods]
 
 
-@router.post("/normalization")
+@router.post("/normalization", summary="Perform normalization")
 async def perform_normalization(normalizations: list[NormalizationType]):
+    """
+    ## Apply normalization to the dataset.
+
+    All available normalization methods can be found at `/normalization/methods`.
+    """
     methods = [normalization.name for normalization in normalizations]
     DataSet().data = DataTransformer.normalize(DataSet().data, methods)
 
 
-@router.get("/data_types", response_model=list[DataType])
+@router.get("/data_types", summary="Data types", response_model=list[DataType])
 async def get_data_types():
+    """
+    ## Retrieve all possible data types.
+    """
     return list(DataType)
 
 
-@router.get("/clustering/graph")
+@router.post("/clustering/graph", summary="Clustering graph")
 async def perform_clustering(method: ClusteringMethod):
+    """
+    ## Generate a clustering graph.
+
+    Applies chosen clustering method to the data, and returns plot of clustering performed with the method.
+    Possible methods can be found at `/clustering/methods`.
+    """
     graph = Clustering.perform_clustering(DataSet().data, method)
     return Response(graph.getvalue(), media_type='image/png')
 
 
-@router.get("/clustering/methods", response_model=list[ClusteringMethod])
-async def get_clustering():
+@router.get("/clustering/methods", summary="Clustering methods", response_model=list[ClusteringMethod])
+async def get_clustering_methods():
+    """
+    ## List available clustering methods.
+    """
     return Clustering.get_clustering_methods()
--- backend/api/schemas.py
+++ backend/api/schemas.py
@@ -33,54 +33,118 @@ clusteringDescription = {
     }
 """
 import datetime
+from typing import List, Union, Dict
 
 from fastapi import File
 from pandas import DataFrame
-from pydantic import BaseModel, TypeAdapter
+from pydantic import BaseModel, Field
 
-from backend.clustering import ClusteringMethod
 from backend.data_type import DataType
 from backend.normalization import CatNormType, NumNormType
 
 
 class Column(BaseModel):
-    name: str
-    type: DataType
-    values: list[float | int | bool | str | datetime.datetime]
+    name: str = Field(..., description="The name of the column.")
+    type: DataType = Field(..., description="The data type of the column.")
+    values: List[Union[float, int, bool, str, datetime.datetime]] = Field(...,
+                                                                          description="A list of values contained in "
+                                                                                      "the column."
+                                                                          )
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "name": "age",
+                "type": "numerical",
+                "values": [25, 30, 45, 60]
+            }
+        }
 
 
 class DatasetSchema(BaseModel):
-    name: str
-    variables: list[Column]
+    name: str = Field(..., description="The name of the dataset.")
+    variables: List[Column] = Field(..., description="A list of columns that make up the dataset.")
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "name": "from data_frame",
+                "variables": [
+                    {
+                        "name": "age",
+                        "type": "numerical",
+                        "values": [25, 30, 45, 60]
+                    },
+                    {
+                        "name": "gender",
+                        "type": "categorical",
+                        "values": ["male", "female", "female", "male"]
+                    }
+                ]
+            }
+        }
 
     @staticmethod
     def from_data_frame(data: DataFrame) -> 'DatasetSchema':
-        variables = [Column(name = column_name,
-                            type = DataType.CATEGORICAL if data[column_name].dtype == 'object' else DataType.NUMERICAL,
-                            values = data[column_name].to_list()) 
-                            for column_name in data.columns]
-        return DatasetSchema(name='from data_frame',variables=variables)
-
-
-DataTypeAdapter = TypeAdapter(DataType)
+        variables = [Column(name=column_name,
+                            type=DataType.CATEGORICAL if data[column_name].dtype == 'object' else DataType.NUMERICAL,
+                            values=data[column_name].to_list())
+                     for column_name in data.columns]
+        return DatasetSchema(name='from data_frame', variables=variables)
 
 
 class NormalizationType(BaseModel):
-    name: CatNormType | NumNormType
-    compatible_types: list[DataType]
+    name: CatNormType | NumNormType = Field(..., description="The name of the normalization method.")
+    compatible_types: List[DataType] = Field(...,
+                                             description="A list of data types compatible with this normalization "
+                                                         "method."
+                                             )
 
-
-ClusteringAlgorithm = TypeAdapter(ClusteringMethod)
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "name": "one_hot",
+                "compatible_types": ["categorical"]
+            }
+        }
 
 
 class Graph(BaseModel):
-    name: str
-    data: bytes = File(...)
+    name: str = Field(..., description="The name of the graph.")
+    data: bytes = File(..., description="The binary data of the graph image.")
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "name": "pca_components",
+                "data": "plot in a binary form"
+            }
+        }
 
 
 class UpdateColumnNames(BaseModel):
-    mapping: dict[str, str]
+    mapping: Dict[str, str] = Field(..., description="A mapping of old column names to new column names.")
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "mapping": {
+                    "old_column_name1": "new_column_name1",
+                    "old_column_name2": "new_column_name2"
+                }
+            }
+        }
 
 
 class UpdateColumnTypes(BaseModel):
-    mapping: dict[str, DataType]
+    mapping: Dict[str, DataType] = Field(..., description="A mapping of column names to their new data types.")
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "mapping": {
+                    "column_name1": "numerical",
+                    "column_name2": "categorical"
+                }
+            }
+        }
