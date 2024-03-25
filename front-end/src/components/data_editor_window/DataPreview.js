ec4c2f4385643f660ce5f9c65ee613a250495633
brzep <brzeppp@gmail.com>
2024-03-25 02:22:02 +0100

    select menu shows proper datatype

--- front-end/src/components/data_editor_window/DataPreview.js
+++ front-end/src/components/data_editor_window/DataPreview.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Papa from "papaparse";
 import { inactiveButtonStyle } from "../../common/styles";
 
 
@@ -8,8 +7,6 @@ const ROWS = 10
 const DataPreview = () => {
     const [data, setData] = useState([]);
 
-    // TODO - upload file from backend
-    // currently loading from disk sample/data.csv
     useEffect(() => {
         const requestDataset = fetch(`http://localhost:8000/api/dataset?rows=${ROWS}`)
             .then(res => res.json())
@@ -46,6 +43,7 @@ const DataPreview = () => {
         console.log("Saving...");
     };
 
+    const variables = data['dataset'] !== undefined ? data['dataset']['variables'] : []
     const headers = data['dataset'] !== undefined ? data['dataset']['variables'].map(variable => variable['name']) : []
     const values = [];
     if (data['dataset'] !== undefined) {
@@ -82,16 +80,17 @@ const DataPreview = () => {
 
                         {/* Data type dropdown*/}
                         <tr className="bg-gray-100">
-                            {headers.map((header, columnId) => (
+                            {variables.map((variable,columnId) => (
                                 <th
-                                    key={`type-${header}`}
+                                    key={`type-${variable['name']}`}
                                     className="px-3 py-2 text-sm text-gray-600"
                                 >
                                     <select
                                         className="form-select block w-full px-2 py-1.5 text-xs font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                         onChange={(event) =>
-                                            changeDataType(header, columnId, event)
+                                            changeDataType(variable['name'], columnId, event)
                                         }
+                                        defaultValue={variable['type']}
                                     >
                                         {dataTypes.map((type) => (
                                             <option key={type} value={type}>
