3d6530632964fe91898d795df0a70f53fd7115aa
Aleksandra Smela <aleksandra.smela@gmail.com>
2024-04-04 20:51:24 +0200

    Add data preview to main menu

---
+++ front-end/src/components/data_preview/DataPreview.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+
+const ROWS = 10;
+
+const DataPreview = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const requestDataset = fetch(
+      `http://localhost:8000/api/dataset?rows=${ROWS}`
+    ).then((res) => res.json());
+
+    Promise.all([requestDataset]).then(([dataset]) => {
+      setData({
+        dataset: dataset,
+      });
+    });
+  }, []);
+
+  const variables =
+    data["dataset"] !== undefined ? data["dataset"]["variables"] : [];
+  const headers =
+    data["dataset"] !== undefined
+      ? data["dataset"]["variables"].map((variable) => variable["name"])
+      : [];
+  const values = [];
+  if (data["dataset"] !== undefined) {
+    for (let i = 0; i < ROWS; i++) {
+      values.push([]);
+    }
+    for (let variable of data["dataset"]["variables"]) {
+      let i = 0;
+      for (let value of variable["values"]) {
+        values[i].push(value);
+        i++;
+      }
+    }
+  }
+  const dataTypes = data["datatypes"] !== undefined ? data["datatypes"] : [];
+
+  return (
+    <div className="relative h-96">
+      <div className="max-h-80 overflow-scroll bg-slate-100">
+        <table className="divide-y divide-gray-200">
+          {/* Headers/Variable names */}
+          <thead className="bg-gray-50">
+            <tr>
+              {headers.map((header, columnId) => (
+                <th
+                  key={header}
+                  className="min-w-32 px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer hover:bg-gray-100 hover:text-gray-700"
+                >
+                  {header}
+                </th>
+              ))}
+            </tr>
+
+            {/* Data type*/}
+            <tr className="bg-gray-100">
+              {variables.map((variable, columnId) => (
+                <th
+                  key={`type-${variable["name"]}`}
+                  className="px-3 py-2 text-sm text-gray-600"
+                >
+                  {variable["type"]}
+                </th>
+              ))}
+            </tr>
+          </thead>
+
+          {/* Data values */}
+          <tbody className="bg-white divide-y divide-gray-200">
+            {values.map((row, index) => (
+              <tr
+                key={index}
+                className={`${index % 2 === 0 ? "bg-blue-50" : "bg-gray-50"}`}
+              >
+                {row.map((value, id) => (
+                  <td
+                    key={id}
+                    className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 text-center"
+                  >
+                    {value}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default DataPreview;
--- front-end/src/pages/MainMenu.js
+++ front-end/src/pages/MainMenu.js
@@ -1,45 +1,34 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { defaultButtonStyle, inactiveButtonStyle } from "../common/styles";
+import DataPreview from "../components/data_preview/DataPreview";
 
 const MainMenu = () => {
   const navigate = useNavigate();
 
-  const handleDataPreview = () => {
-    // TODO - implement sending header name and data type changes to back
-    // send changes that are kept in context
-    console.log("DataPreview...");
-  };
-
   return (
-    <div className="left-0 right-0 flex justify-center items-center">
-      <div className="relative h-[100px] w-[400px]">
-        <div className="left-0 right-0 flex justify-center items-center">
-          <button
-            onClick={() => navigate("/pca-main-components")}
-            className={defaultButtonStyle + "relative w-[150px]"}
-            style={{ margin: "auto" }}
-          >
-            PCA
-          </button>
-          <button
-            onClick={() => navigate("/clusterization-options")}
-            className={defaultButtonStyle + "relative w-[150px]"}
-            style={{ margin: "auto" }}
-          >
-            Klastryzacja
-          </button>
-        </div>
-        <div className="absolute bottom-0 left-0 right-0 flex justify-center items-center">
-          <button
-            onClick={handleDataPreview}
-            className={inactiveButtonStyle}
-            style={{ margin: "auto" }}
-          >
-            Pokaż aktualne dane
-          </button>
+    <div>
+      <div className="left-0 right-0 flex justify-center items-center">
+        <div className="relative h-[100px] w-[400px]">
+          <div className="left-0 right-0 flex justify-center items-center">
+            <button
+              onClick={() => navigate("/pca-main-components")}
+              className={defaultButtonStyle + "relative w-[150px]"}
+              style={{ margin: "auto" }}
+            >
+              PCA
+            </button>
+            <button
+              onClick={() => navigate("/clusterization-options")}
+              className={defaultButtonStyle + "relative w-[150px]"}
+              style={{ margin: "auto" }}
+            >
+              Klastryzacja
+            </button>
+          </div>
         </div>
       </div>
+      <DataPreview />
     </div>
   );
 };
