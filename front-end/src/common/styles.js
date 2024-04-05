3e477152bb6c1f5d315f9d850251931622ece488
Kaja Dudek <410170@student.agh.edu.pl>
2024-04-05 20:44:32 +0200

    Button styling fixed

--- front-end/src/common/styles.js
+++ front-end/src/common/styles.js
@@ -1,6 +1,6 @@
 // buttons
 var defaultButtonStyle =
-  " text-white bg-main-medium hover:bg-main-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800";
+  " text-white bg-main-medium hover:bg-main-800 focus:ring-4 focus:ring-main-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800";
 
 var inactiveButtonStyle =
   " text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800";
