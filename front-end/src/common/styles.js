0d47d90988c1b4ef620891d5a607860baf5be243
Kaja Dudek <410170@student.agh.edu.pl>
2024-04-05 10:55:04 +0200

    styling dropdown

--- front-end/src/common/styles.js
+++ front-end/src/common/styles.js
@@ -1,3 +1,4 @@
+// buttons
 var defaultButtonStyle =
   " text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800";
 
@@ -5,6 +6,10 @@ var inactiveButtonStyle =
   " text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800";
 
 var navigationButtonStyle =
-  "flex items-center justify-center px-4 py-2 bg-blue-700 text-white rounded-lg shadow-md hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-800 focus:ring-opacity-50";
+  "flex items-center justify-center px-4 py-2 bg-main-medium text-white rounded-lg shadow-md hover:bg-main-800 focus:outline-none focus:ring-2 focus:ring-main-800 focus:ring-opacity-50";
 
-export { defaultButtonStyle, inactiveButtonStyle, navigationButtonStyle };
+// other
+var dropdownListStyle =
+  "form-select block w-full px-2 py-1.5 text-xs font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-main-light focus:outline-none cursor-pointer"
+
+export { defaultButtonStyle, inactiveButtonStyle, navigationButtonStyle, dropdownListStyle };
