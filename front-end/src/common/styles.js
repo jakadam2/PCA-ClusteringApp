466204a663259fac335239336a6aca12bdd30d6e
Aleksandra Smela <aleksandra.smela@gmail.com>
2024-04-08 01:01:35 +0200

    Style remaining buttons

--- front-end/src/common/styles.js
+++ front-end/src/common/styles.js
@@ -1,21 +1,13 @@
 // buttons
-var defaultButtonStyle =
-  " text-white bg-main-medium hover:bg-main-800 focus:ring-4 focus:ring-main-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800";
-
-var inactiveButtonStyle =
-  " text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800";
-
 var navigationButtonStyle =
   "flex items-center justify-center px-4 py-2 bg-main-medium text-white rounded-lg shadow-md hover:bg-main-800 focus:outline-none focus:ring-2 focus:ring-main-800 focus:ring-opacity-50";
 
 // data table
-var dataHeaderStyle = 
-  "min-w-32 max-w-40 h-12 px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider hover:bg-main-100 hover:text-gray-700"
+var dataHeaderStyle =
+  "min-w-32 max-w-40 h-12 px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider hover:bg-main-100 hover:text-gray-700";
 
 // other
 var dropdownListStyle =
-  "form-select block w-full px-2 py-1.5 text-xs font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-main-light focus:outline-none cursor-pointer"
+  "form-select block w-full px-2 py-1.5 text-xs font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-main-light focus:outline-none cursor-pointer";
 
-export { defaultButtonStyle, inactiveButtonStyle, navigationButtonStyle, 
-  dataHeaderStyle,
-  dropdownListStyle };
+export { navigationButtonStyle, dataHeaderStyle, dropdownListStyle };
