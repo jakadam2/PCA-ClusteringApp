38efbc3da281625b9c87473d142b726a7a376416
brzep <brzeppp@gmail.com>
2024-04-09 00:53:07 +0200

    Launching backend exe from React

--- back-end/app.py
+++ back-end/app.py
@@ -22,5 +22,6 @@ app.add_middleware(
 
 
 if __name__ == "__main__":
+    print("backend here")
     multiprocessing.freeze_support()
     uvicorn.run(app, host="127.0.0.1", port=8000,log_level="debug",reload=False,workers = 1)
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,21 +9,32 @@ import ClusterizationOptions from './pages/ClusterizationOptions';
 import ClusterizationVisualization from './pages/ClusterizationVisualization';
 
 function App() {
-  return (
-    <div className="px-10 py-10 justify-start min-h-screen">
-      <Router>
-        <Routes>
-          <Route path="/" element={<FileExplorer/>} />
-          <Route path="/menu" element={<MainMenu/>} />
-          <Route path="/data-type-edit" element={<DataTypeEditPage/>} />
-          <Route path="/save-file" element={<FileSaver/>} />
-          <Route path="/pca-main-components" element={<MainPCAComponentsVisualization/>} />
-          <Route path="/clusterization-options" element={<ClusterizationOptions/>} />
-          <Route path="/clusterization-visualization" element={<ClusterizationVisualization/>} />
-        </Routes>
-      </Router>
-    </div>
-  );
+    const path = require("path")
+    const { execFile } = window.require("child_process")
+    var exePath = path.resolve('./backend.exe')
+    var python = execFile(exePath, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`execFile error: ${error}`);
+            return;
+        }
+    console.log(`>>>>>>>>>>>>>>>> ${stdout}`);
+    });
+    console.log("done");
+    return (
+        <div className="px-10 py-10 justify-start min-h-screen">
+        <Router>
+            <Routes>
+            <Route path="/" element={<FileExplorer/>} />
+            <Route path="/menu" element={<MainMenu/>} />
+            <Route path="/data-type-edit" element={<DataTypeEditPage/>} />
+            <Route path="/save-file" element={<FileSaver/>} />
+            <Route path="/pca-main-components" element={<MainPCAComponentsVisualization/>} />
+            <Route path="/clusterization-options" element={<ClusterizationOptions/>} />
+            <Route path="/clusterization-visualization" element={<ClusterizationVisualization/>} />
+            </Routes>
+        </Router>
+        </div>
+    );
 }
 
 export default App;
