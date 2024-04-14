e17d5d591e01f2bf9fa21a788a812ff00435fe7a
Kaja Dudek <410170@student.agh.edu.pl>
2024-04-14 18:14:36 +0200

    Styling `FileSaver`

--- front-end/src/components/file_utils/FileSaver.js
+++ front-end/src/components/file_utils/FileSaver.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { navigationButtonStyle } from "../../common/styles";
+import Title from "../Title";
 
 const FileSaver = () => {
   const [directoryHandle, setDirectoryHandle] = useState(null);
@@ -69,16 +71,14 @@ const FileSaver = () => {
 
   return (
     <div>
-      <p className="mb-5">Wybierz miejsce docelowe do zapisu pliku:</p>
+      <Title title="Zapisz plik"/>
       {directoryHandle ? (
         <>
           <div className="flex flex-col items-start w-full mb-2">
             <div className="flex gap-2 w-full">
               <button
                 onClick={selectDirectory}
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 
-                                    font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 
-                                    focus:outline-none dark:focus:ring-blue-800 w-36"
+                className={navigationButtonStyle}
               >
                 Wybierz folder
               </button>
@@ -87,18 +87,16 @@ const FileSaver = () => {
                 value={fileName}
                 onChange={handleFileNameChange}
                 placeholder="Nazwa pliku"
-                className="flex-grow text-sm p-2.5 border rounded-lg focus:ring-blue-500 focus:border-blue-500"
+                className="flex-grow text-sm p-2.5 border rounded-lg focus:outline-none"
               />
             </div>
-            <div className="flex justify-between items-center w-full mt-2">
+            <div className="flex justify-between items-top w-full mt-2">
               <p className="text-gray-500 text-sm">
                 Wybrany folder: {directoryName}
               </p>
               <button
                 onClick={saveFileToDirectory}
-                className="text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 focus:ring-blue-300 
-                                    font-small rounded-lg text-sm px-3 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 
-                                    focus:outline-none dark:focus:ring-blue-800 w-24"
+                className={navigationButtonStyle}
               >
                 Zapisz plik
               </button>
@@ -106,14 +104,14 @@ const FileSaver = () => {
           </div>
         </>
       ) : (
-        <button
-          onClick={selectDirectory}
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 
-                        font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 
-                        focus:outline-none dark:focus:ring-blue-800"
-        >
-          Wybierz folder
-        </button>
+        <div className="flex justify-center items-center">
+          <button
+            onClick={selectDirectory}
+            className={navigationButtonStyle}
+          >
+            Wybierz folder
+          </button>
+        </div>
       )}
     </div>
   );
